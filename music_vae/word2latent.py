bca0ce56b6acf0c44bbbd84b86f743c3aa1b371d
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-11-25 15:03:18 +0800

    updated word2latent.py

--- music_vae/word2latent.py
+++ music_vae/word2latent.py
@@ -7,4 +7,26 @@ import tempfile
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers
 
+# TODO: find out the dimensions of inputs and outputs
+wordvec_length = -1
+latentvec_length = -1
+
+dense_layer_size = -1
+dropout_rate = 0.2
+
+# TODO: add the dataset
+wordvec_batch = []
+latentvec_batch = []
+epochs = -1
+
+model = tf.keras.models.Sequential([
+    layers.Dense(dense_layer_size, activation = 'relu', input_dim = wordvec_length),
+    layers.Dropout(dropout_rate),
+    layers.Dense(latentvec_length) # output layer
+])
+
+model.compile(optimizer = 'adam', loss = 'MSE', metrics = ['accuracy'])
+
+model.fit(wordvec_batch, latentvec_batch, epochs = epochs)
