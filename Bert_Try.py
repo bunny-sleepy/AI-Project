c9454ecb4c66e1194030423371f1d715e1ea84a0
Li-Zhouzi <61537585+Li-Zhouzi@users.noreply.github.com>
2020-12-01 20:24:15 +0800

    Add files via upload

---
+++ Bert_Try.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import BertTokenizer, BertModel
+
+Tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+Model = BertModel.from_pretrained('bert-base-uncased')
+
+def encode_nlp(text,model,tokenizer):
+    tokenized_text = tokenizer.tokenize(text)
+    indexed_tokens=tokenizer.convert_tokens_to_ids(tokenized_text)
+    tokens_tensor = torch.tensor([indexed_tokens])
+    model.eval()
+#    tokens_tensor = tokens_tensor.to('cuda')
+#    model.to('cuda')
+
+    with torch.no_grad():
+        outputs = model(tokens_tensor)
+        encoded_layers = outputs[0]
+    assert tuple(encoded_layers.shape) == (1, len(indexed_tokens), model.config.hidden_size)
+    output = encoded_layers.numpy()[0]
+
+    return output
+
+
+print(encode_nlp("Hello world!", Model, Tokenizer))
