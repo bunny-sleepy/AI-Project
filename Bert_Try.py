121ee6e70cf9a1961f93c1e3765344a06f9be874
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-01 22:51:01 +0800

    omitted

--- Bert_Try.py
+++ Bert_Try.py
@@ -1,10 +1,11 @@
 import torch
 from transformers import BertTokenizer, BertModel
+import numpy as np
 
 Tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 Model = BertModel.from_pretrained('bert-base-uncased')
 
-def encode_nlp(text, model, tokenizer):
+def encode_nlp(text, model = Model, tokenizer = Tokenizer):
     tokenized_text = tokenizer.tokenize(text)
     indexed_tokens=tokenizer.convert_tokens_to_ids(tokenized_text)
     tokens_tensor = torch.tensor([indexed_tokens])
@@ -17,8 +18,6 @@ def encode_nlp(text, model, tokenizer):
         encoded_layers = outputs[0]
     assert tuple(encoded_layers.shape) == (1, len(indexed_tokens), model.config.hidden_size)
     output = encoded_layers.numpy()[0]
+    return np.array(output)
 
-    return output
-
-
-print(encode_nlp("Hello world!", Model, Tokenizer))
+# print(len(encode_nlp("Hello world!", Model, Tokenizer)))
