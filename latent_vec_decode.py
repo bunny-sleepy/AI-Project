1fdedaaf102aa75ecf62a579cdbd4eb06e331cef
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-02 11:17:14 +0800

    Added midi output method to latent_vec_decode.py

--- latent_vec_decode.py
+++ latent_vec_decode.py
@@ -2,9 +2,12 @@ from music_vae.trained_model import TrainedModel
 import note_seq
 import os, sys
 import music_vae.configs as configs
+import time
 
 class NoExtractedExamplesError(Exception):
   pass
+
+date_and_time = time.strftime('%Y-%m-%d_%H%M%S')
 
 # TODO: find the appropriate batch size
 config_str = 'hierdec-mel_16bar'
@@ -39,3 +42,25 @@ def decode(trained_model = trained_model, z_batch = [], samples_per_batch = 1, t
                 note_seq.extend(ns)
             note_seq_batch.append(note_seq)
     return note_seq_batch
+
+def decode_to_midi(target_directory, trained_model = trained_model, z_batch = [], samples_per_batch = 1, temperature = 0.5):
+    """ decode the generated z into note sequences
+
+    Args:
+        target_directory: the directory to hold the generated piece.
+        trained_model: the trained model, may be loaded from checkpoints or
+          use the music_vae.trained_model.TrainedModel method.
+        z_batch: the input batch of z, each np.array generates one output.
+        samples_per_batch: how many note sequences to generate for each z.
+        temperature: softmax temperature used in model.decode.
+
+    Return:
+        a batch of note sequences.
+    """
+    note_seq_batch = decode(trained_model, z_batch, samples_per_batch, temperature)
+    basename = os.path.join(
+        target_directory,
+        '%s_%03d.mid' %
+        (date_and_time, samples_per_batch))
+    for noteseq in note_seq_batch:
+        note_seq.sequence_proto_to_midi_file(noteseq, basename.replace('*', '%03d' % i))
