9b00ccbe03c0c7961f2be985b6ac0502192cf1c0
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-04 23:30:26 +0800

    Some modifications

--- preprocessing/preprocess_midi.py
+++ preprocessing/preprocess_midi.py
@@ -1,11 +1,48 @@
 import note_seq
 import os, sys
+import numpy as np
 
+# test_target = './midi_input/BackToDecember.mid'
 test_target = './midi_input/SomebodyThatIUsedToKnow(BetterVersion).mid'
 # test_target = './midi_input/7Days.mid'
 file = open('note_seq3.txt', 'w+')
 
 ns = note_seq.midi_file_to_note_sequence(test_target)
+
+def avg_velocity(ns):
+    number = {}
+    for note in ns.notes:
+        if not note.is_drum:
+            if note.instrument in number.keys():
+                number[note.instrument].append(note.velocity)
+            else:
+                number[note.instrument] = [note.velocity]
+    for instrument in number:
+        mean_velocity = np.mean(number[instrument])
+        number[instrument] = mean_velocity
+    max_instrument = -1
+    max_pitch_avg = -1.0
+    for instrument in number:
+        if number[instrument] > max_pitch_avg:
+            max_instrument = instrument
+            max_pitch_avg = number[instrument]
+    return max_instrument, number
+
+def total_time(ns):
+    number = {}
+    for note in ns.notes:
+        if not note.is_drum:
+            if note.instrument in number.keys():
+                number[note.instrument].append(note.end_time - note.start_time)
+            else:
+                number[note.instrument] = [note.end_time - note.start_time]
+    max_instrument = -1
+    max_total_time = -1.0
+    for instrument in number:
+        if np.sum(number[instrument]) > max_total_time:
+            max_instrument = instrument
+            max_total_time = np.sum(number[instrument])
+    return max_instrument, number
 
 # According to the maximum pitch difference
 def max_pitch_diff(ns):
@@ -67,6 +104,8 @@ def max_diversity(ns):
     return max_instrument, number
 
 def get_new_ns(max_instrument):
+    if max_instrument is None:
+        return None
     seq = note_seq.NoteSequence()
     seq.tempos.add().qpm = ns.tempos[0].qpm
     seq.ticks_per_quarter = ns.ticks_per_quarter
@@ -99,13 +138,31 @@ def skyline(ns):
     pitch_diff = max_pitch_diff(ns)[1]
     pitch_max = max_pitch(ns)[1]
     pitch_diversity = max_diversity(ns)[1]
-    print(pitch_max)
-    print(pitch_diff)
-    print(pitch_diversity)
-    pick_num = 3
+    pitch_total_time = total_time(ns)[1]
+    # print(pitch_max)
+    # print(pitch_diff)
+    # print(pitch_diversity)
+    pick_num = len(pitch_max) / 3
+    pick_num_max = len(pitch_max) * 3 / 4
     top_diff = []
     top_max = []
     top_diversity = []
+    top_total_time = []
+
+    for instrument in pitch_total_time:
+        if len(top_total_time) < pick_num:
+            top_total_time.append(instrument)
+        else:
+            min_index = -1
+            min_total_time = 10000.0
+            for ins in top_total_time:
+                curr_total_time = np.sum(pitch_total_time[ins])
+                if curr_total_time < min_total_time:
+                    min_total_time = curr_total_time
+                    min_index = top_total_time.index(ins)
+            if np.sum(pitch_total_time[instrument]) > min_total_time:
+                top_total_time[min_index] = instrument
+
     for instrument in pitch_diff:
         if len(top_diff) < pick_num:
             top_diff.append(instrument)
@@ -121,7 +178,7 @@ def skyline(ns):
                 top_diff[min_index] = instrument
 
     for instrument in pitch_max:
-        if len(top_max) < pick_num:
+        if len(top_max) < pick_num_max:
             top_max.append(instrument)
         else:
             min_index = -1
@@ -131,7 +188,7 @@ def skyline(ns):
                 if curr_max < min_max:
                     min_max = curr_max
                     min_index = top_max.index(ins)
-            if pitch_diff[instrument][1] - pitch_diff[instrument][0] > min_max:
+            if pitch_max[instrument] > min_max:
                 top_max[min_index] = instrument
 
     for instrument in pitch_diversity:
@@ -139,30 +196,39 @@ def skyline(ns):
             top_diversity.append(instrument)
         else:
             min_index = -1
-            min_diff = 10000
+            min_diversity = 10000
             for ins in top_diversity:
-                curr_diff = pitch_diff[ins][1] - pitch_diff[ins][0]
-                if curr_diff < min_diff:
-                    min_diff = curr_diff
+                curr_diversity = len(pitch_diversity[ins])
+                if curr_diversity < min_diversity:
+                    min_diversity = curr_diversity
                     min_index = top_diversity.index(ins)
-            if pitch_diff[instrument][1] - pitch_diff[instrument][0] > min_diff:
+            if len(pitch_diversity[instrument]) > min_diversity:
                 top_diversity[min_index] = instrument
-    print(top_diff, top_diversity, top_max)
+
+    print(top_diff, top_diversity, top_max, top_total_time)
     target = []
-    for instrument in top_diversity:
-        if (instrument in top_max) and (instrument in top_diff):
-            target.append(instrument)
-    if len(target) == 0:
-        return None
-    target_instrument = target[0]
+    # for instrument in top_diversity:
+    #     if (instrument in top_max) and (instrument in top_diff) and (instrument in top_total_time):
+    #         target.append(instrument)
+    # if len(target) == 0:
+    #     return None
+    # target_instrument = target[0]
+    target.extend(top_diff)
+    target.extend(top_diversity)
+    target.extend(top_max)
+    target.extend(top_total_time)
+
+    counts = np.bincount(target)
+    target_instrument = np.argmax(counts)
     return target_instrument
 
 def main():
-    try:
-        seq = get_new_ns(skyline(ns))
-        note_seq.sequence_proto_to_midi_file(seq, './midi_output/preprocess_output/out%s.mid' % '1')
-    except:
+    notes = skyline(ns)
+    if notes is None:
         print('No track selected')
+    else:
+        seq = get_new_ns(notes)
+        note_seq.sequence_proto_to_midi_file(seq, './midi_output/preprocess_output/out%s.mid' % '1')
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,27 +1,34 @@
+import os
 import latent_vec_decode, latent_vec_generate
-from music_vae.trained_model import TrainedModel
-import music_vae.configs as configs
-import numpy as np
+path = 'C:/Users/Li/PycharmProjects/AI_proj/AI-proj/midids'
 
-# TODO: find the appropriate batch size
-config_str = 'hierdec-mel_16bar'
-config = configs.CONFIG_MAP[config_str]
-config.data_converter.max_tensors_per_item = None
-batch_size = 8
-# NOTE: you should change your directory HERE
-checkpoint_dir = 'D:/code/Github/repository/musicvae_hierdec-mel_16bar'
-trained_model = TrainedModel(config,
-                             batch_size,
-                             checkpoint_dir_or_path = checkpoint_dir)
+list_name=[]
+list_midi=[]
+list_vector=[]
+list_midiname=[]
+for file in os.listdir(path):  
+        file_path = path + '/' + file 
+        if os.path.splitext(file_path)[1]=='.mid':  
+            list_name.append(file_path)
+            list_midi.append(file_path)
+            list_midiname.append(file)
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+#print(list_midiname)
+encoded_list = latent_vec_generate.encode(midi_batch=list_midi)
+for m in list_midiname:
+    z,u,v = encoded_list[m]
+    list_vector.append(z)
 
-# test on the decode test1.mid
-test_file_path = './midi_input/test1.mid'
-z, mu, sigma = latent_vec_generate.encode(trained_model = trained_model,
-                                          midi_batch=[test_file_path])['test1.mid']
-target_directory = './midi_output/decode_output'
-latent_vec_decode.decode_to_midi(
-    target_directory,
-    trained_model = trained_model,
-    length = len(z),
-    z_batch = [z])
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+
+path1 = 'C:/Users/Li/PycharmProjects/AI_proj/AI-proj/testback/latentback'
+path2 = 'C:/Users/Li/PycharmProjects/AI_proj/AI-proj/testback/nameback'
+with open(path1, 'w+') as file_object:
+    file_object.write(str(list_vector))
+with open(path2, 'w+') as file_object:
+    file_object.write(str(list_midiname))
+
+#print(s)
+#target_directory = 'C:/Users/Li/PycharmProjects/AI_proj/AI-proj/testback'
+#latent_vec_decode.decode_to_midi(target_directory, length = len(s), z_batch = [s])
 
