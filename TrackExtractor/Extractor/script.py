4cee316a16a8978d65aa3b3efd154b9defecbb26
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-09 16:23:31 +0800

    Combined the two methods in midi preprocessing Plz check its performance

--- TrackExtractor/Extractor/script.py
+++ TrackExtractor/Extractor/script.py
@@ -1,14 +1,29 @@
 import numpy as np
 from track_identifier import proc
 from miditoolkit.midi import parser
+import preprocessing.preprocess_midi as pm
+import note_seq
 
 # save only melodic track
 # path_midi = 'test_midis/aladdin-medley-of-all-songs-mid.mid'
-path_midi = 'test_midis/SomebodyThatIUsedToKnow.mid'
-midi_file = parser.MidiFile(path_midi)
-ys = proc.identify_song(midi_file)
 
-print(ys)
-midx = list(np.where(np.array(ys)==0)[0])
-print(midx)
-midi_file.dump(filename='melody.mid', instrument_idx=midx)
+def preprocess(path_midi, dump_path = 'tmp.mid'):
+    midi_file = parser.MidiFile(path_midi)
+    ys = proc.identify_song(midi_file)
+    midx = list(np.where(np.array(ys)==0)[0])
+    midi_file.dump(filename = dump_path, instrument_idx = midx)
+
+def main():
+    path_mid = 'test_midis/SomebodyThatIUsedToKnow.mid'
+    dump_path = 'tmp.mid'
+    # algo according to script.py
+    preprocess(path_mid, dump_path)
+    # algo according to filters
+    ns = note_seq.midi_file_to_note_sequence(dump_path)
+    new_ns = pm.get_new_ns(pm.skyline(ns), ns)
+    # save the output
+    save_path = 'out.mid'
+    note_seq.sequence_proto_to_midi_file(new_ns, save_path)
+
+if __name__ == "__main__":
+    main()
