171033bf75fe1dce3b27feee62b948b7f9011d14
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-09 22:57:05 +0800

    Update word2latent_torch.py

--- word2latent_torch.py
+++ word2latent_torch.py
@@ -4,9 +4,10 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as Data
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torchvision import datasets, transforms
 
-fc_size = 1024
+fc_size = 1000
 
 class net(nn.Module):
     def __init__(self):
@@ -26,8 +27,8 @@ def train_model(wordvec_batch = None,
                 latentvec_batch = None,
                 dense_layer_size = 2048,
                 dropout_rate = 0.2,
-                learning_rate = 1e-1,
-                momentum = 0.9,
+                learning_rate = 1e-2,
+                momentum = 0.5,
                 epochs = 1000,
                 wordvec_length = 768,
                 latentvec_length = 512,
@@ -52,16 +53,36 @@ def train_model(wordvec_batch = None,
     """
     # TODO: evaluate the effectiveness of this model
     # TODO: save the trained model with training time
+    dataset = TensorDataset(wordvec_batch, latentvec_batch)
+    train_loader = DataLoader(dataset=dataset,
+                          batch_size=32,
+                          shuffle=True)
     model = None
     if train:
         model = net()
-        optimizer = optim.SGD(model.parameters(), lr = learning_rate, momentum = momentum)
-        criterion = nn.CrossEntropyLoss()
+        #optimizer = optim.SGD(model.parameters(), lr = learning_rate, momentum = momentum)
+        optimizer = optim.Adam(model.parameters())
+        criterion = nn.MSELoss()
         for epoch in range(epochs):
-            out = model(wordvec_batch)
-            loss = criterion(out, latentvec_batch)
-            loss.backward()
-            print('Train Epoch: {} \tLoss: {:.6f}'.format(
-                epoch, loss.item()))
-            optimizer.step()
+            for batch_idx, (data, target) in enumerate(train_loader):
+                output = model(data)
+                loss = criterion(output, target)
+                if batch_idx % 50 == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                            100. * batch_idx / len(train_loader), loss.item()))
+                optimizer.zero_grad()
+                loss.backward(retain_graph=True)
+                optimizer.step()
+            # for i in range(len(wordvec_batch)):
+            #     out = model(wordvec_batch[i])
+            #     loss = criterion(out, latentvec_batch[i])
+            #     print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, loss.item()))
+            #     optimizer.zero_grad()
+            #     loss.backward(retain_graph=True)
+            #     optimizer.step()
+    elif not os.path.exists(checkpoint_path):
+        print("ERROR: No existing checkpoint")
+    else:
+        model = tf.keras.models.load_model(checkpoint_path)
     return model
