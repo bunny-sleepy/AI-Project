6247adea159c321814c04a36332a77a5685a940f
bunny-sleepy <61811486+bunny-sleepy@users.noreply.github.com>
2020-12-11 21:48:06 +0800

    Update script.py

--- track_extractor/extractor/script.py
+++ track_extractor/extractor/script.py
@@ -1,7 +1,7 @@
 import numpy as np
 from track_identifier import proc
 from miditoolkit.midi import parser
-import preprocess_midi as pm
+import preprocessing.preprocess_midi as pm
 import note_seq
 
 # save only melodic track
@@ -27,7 +27,7 @@ def main():
         note_seq.sequence_proto_to_midi_file(new_ns, save_path)
     except:
         ns = note_seq.midi_file_to_note_sequence(path_mid)
-        new_ns = pm.get_new_ns(pm.skyline(ns), ns)
+        new_ns = pm.get_new_ns(pm.skyline(ns, mode = 'variance_first'), ns)
         save_path = '_out.mid'
         note_seq.sequence_proto_to_midi_file(new_ns, save_path)
 
